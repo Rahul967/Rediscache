f2c57aeefbae615bf7db837c2792f82487669853
Rahul967 <contactrahulkrsingh@gmail.com>
2023-05-22 14:35:05 +0530

    feat: :fire: now we can call redis functions in index.js from redis.utils.js

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 const express = require("express");
 const axios = require("axios");
-const redis = require("redis");
+const { connectToRedis, getFromRedisCache, setInRedisCache } = require("./redis.utils");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-let redisClient;
-
 (async () => {
-  redisClient = redis.createClient();
-
-  redisClient.on("error", (error) => console.error(`Error : ${error}`));
-
-  await redisClient.connect();
+  await connectToRedis();
 })();
 
-
 /**
- * 
- * @param {*} species 
- * @returns {*} species data fetched from the api
+ * @param {*} species
+ * @returns {*} species data fetched from the API
  */
 async function fetchApiData(species) {
   const apiResponse = await axios.get(
@@ -29,40 +20,38 @@ async function fetchApiData(species) {
   return apiResponse.data;
 }
 
-
 /**
- * 
- * @param {*} req  
+ * @param {*} req
  * @param {*} res
- * @returns {*} checks if data returned from the api is empty or not and sends the data to the client else sends an error message
+ * @returns {*} checks if data returned from the API is empty or not and sends the data to the client, otherwise sends an error message
  */
 async function getSpeciesData(req, res) {
-    const species = req.params.species;
-    let results;
-    let isCached = false;
+  const species = req.params.species;
+  let results;
+  let isCached = false;
 
-    try {
-    const cacheResults = await redisClient.get(species);
+  try {
+    const cacheResults = await getFromRedisCache(species);
     if (cacheResults) {
       isCached = true;
-      results = JSON.parse(cacheResults);
+      results = cacheResults;
     } else {
       results = await fetchApiData(species);
       if (results.length === 0) {
         throw "API returned an empty array";
-     }
-     await redisClient.set(species, JSON.stringify(results));
+      }
+      await setInRedisCache(species, results);
     }
 
     res.send({
       fromCache: isCached,
       data: results,
-    })} catch (error) {
-      console.error(error);
-      res.status(404).send("Data unavailable");
-    }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("Data unavailable");
   }
-  
+}
 
 app.get("/fish/:species", getSpeciesData);
 
--- redis.utils.js
+++ redis.utils.js
@@ -1,65 +1,44 @@
+const redis = require("redis");
 
 
+let redisClient;
 
-  async function connectToRedis() {
-    return new Promise((resolve, reject) => {
-        const redisClient = redis.createClient();
+// Function to connect to Redis
+async function connectToRedis() {
+ try{ redisClient = redis.createClient();
+
+  redisClient.on("error", (error) => console.error(`Error: ${error}`));
+
+  const connectAsync = redisClient.connect();
+  await connectAsync;
+ } catch (error) {
+  throw error;    
+}
+}
+
+// Function to get data from Redis cache
+async function getFromRedisCache(key) {
+  try {
   
-      redisClient.on("error", (error) => {
-        console.error(`Error: ${error}`);
-        reject(error);
-      });
-  
-      redisClient.on("connect", () => {
-        console.log("Connected to Redis");
-        resolve(redisClient);
-      });
-    });
+    let cacheData = await redisClient.get(key);
+    return JSON.parse(cacheData);
+  } catch (error) {
+    throw error;    
   }
+}
 
-
-  
-async function getFromRedis(key) {
-    try {
-      return await new Promise((resolve, reject) => {
-        redisClient.get(key, (error, result) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(result);
-          }
-        });
-      });
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+// Function to set data in Redis cache
+async function setInRedisCache(key, data) {
+try{
+  let setAsync = await redisClient.set(key, JSON.stringify(data));
+  await setAsync;
+}  catch (error) {
+    throw error;    
   }
-  
-  async function setToRedis(key, value) {
-    try {
-      return await new Promise((resolve, reject) => {
-        redisClient.set(key, value, (error, result) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(result);
-          }
-        });
-      });
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  }
+}
 
-
-
-
-
-
-    module.exports = {
-        connectToRedis,
-        getFromRedis,
-        setToRedis,
-        };
+module.exports = {
+  connectToRedis,
+  getFromRedisCache,
+  setInRedisCache,
+};
